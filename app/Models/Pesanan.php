e2a106de160634e5c0f15d511b861cf1282089ef
narareya <nomnomhiranami@gmail.com>
2026-07-28 14:54:45 +0700

    fix order

--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pesanan extends Model
 {
-    //
+    protected $fillable = [
+        'nama_pemesan',
+        'no_hp',
+        'bobot',
+        'keterangan',
+        'kuantitas',
+        'total',
+        'alamat_pengiriman',
+    ];
 }
